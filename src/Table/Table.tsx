b24868bcb78cc56843af1c78ab43e63d38f9c003
Lakshya Thakur <lapstjup@gmail.com>
2019-10-02 17:29:18 +0530

    switched from table structure to css grid

--- src/Table/Table.tsx
+++ src/Table/Table.tsx
@@ -48,104 +48,86 @@ export const DRSTable = (props: any) => {
     <DragDropContext onDragEnd={handleDragging}>
       <Droppable droppableId="droppable" direction="horizontal">
         {(provided, snapshot) => (
-          <table className="customTable">
-            <thead
-              ref={provided.innerRef}
-              style={
-                snapshot.isDraggingOver
-                  ? { width: `${parentWidth}px`, background: "white" }
-                  : { width: `${parentWidth}px` }
-              }
-            >
-              <tr>
-                {columns.map((column: any, index: any) => (
-                  <th className={activeColumnn === column.key ? "active" : ""}>
-                    <Draggable
-                      key={column.key}
-                      draggableId={column.key}
-                      isDragDisabled={isDragDisabled}
-                      index={index}
+          <div className="customTable">
+            <div className="thead" ref={provided.innerRef}>
+              {columns.map((column: any, index: any) => (
+                <Draggable
+                  key={column.key}
+                  draggableId={column.key}
+                  isDragDisabled={isDragDisabled}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <div
+                      className={
+                        activeColumnn === column.key
+                          ? "resizableBox active"
+                          : "resizableBox"
+                      }
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={{
+                        width: column.width,
+                        minWidth: minWidth,
+                        ...provided.draggableProps.style
+                      }}
                     >
-                      {(provided, snapshot) => (
-                        <div
-                          className={
-                            activeColumnn === column.key
-                              ? "resizableBox active"
-                              : "resizableBox"
-                          }
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          style={{
-                            width: column.width,
-                            minWidth: minWidth,
-                            ...provided.draggableProps.style
-                          }}
-                        >
+                      <div
+                        onClick={e =>
+                          !snapshot.isDragging
+                            ? handleSorting(column.key)
+                            : e.preventDefault()
+                        }
+                        className="contentBox"
+                      >
+                        <div>
+                          <span>{column.name}</span>
                           <div
-                            onClick={e =>
-                              !snapshot.isDragging
-                                ? handleSorting(column.key)
-                                : e.preventDefault()
-                            }
-                            className="contentBox"
-                          >
-                            <div>
-                              <span>{column.name}</span>
-                              <div
-                                className={
-                                  activeColumnn === column.key
-                                    ? isAsc
-                                      ? "arrow-down"
-                                      : "arrow-up"
-                                    : ""
-                                }
-                              />
-                            </div>
-                          </div>
-                          <div
-                            className="resizeHandle"
-                            onMouseDown={e => handleResizing(e, column, index)}
-                            onMouseOver={e =>
-                              isDragDisabled !== true
-                                ? setDragDisabled(true)
-                                : e.preventDefault()
-                            }
-                            onMouseLeave={e =>
-                              isDragDisabled !== false
-                                ? setDragDisabled(false)
-                                : e.preventDefault()
+                            className={
+                              activeColumnn === column.key
+                                ? isAsc
+                                  ? "arrow-down"
+                                  : "arrow-up"
+                                : ""
                             }
                           />
                         </div>
-                      )}
-                    </Draggable>
-                  </th>
-                ))}
-              </tr>
-            </thead>
-
-            <tbody>
+                      </div>
+                      <div
+                        className="resizeHandle"
+                        onMouseDown={e => handleResizing(e, column, index)}
+                        onMouseOver={e =>
+                          isDragDisabled !== true
+                            ? setDragDisabled(true)
+                            : e.preventDefault()
+                        }
+                        onMouseLeave={e =>
+                          isDragDisabled !== false
+                            ? setDragDisabled(false)
+                            : e.preventDefault()
+                        }
+                      />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+            </div>
+            <div className="tbody" style={{ width: `${parentWidth}px` }}>
               {rows.map((row: any) => (
-                <tr>
+                <div>
                   {Object.keys(row).map((key: any, index: number) => (
-                    <td
+                    <div
+                      style={{ width: `${columns[index].width}px` }}
                       className={activeColumnn === key ? "selectedColumn" : ""}
                     >
-                      <div
-                        style={{ width: `${columns[index].width}px` }}
-                        className={
-                          activeColumnn === key ? "selectedColumn" : ""
-                        }
-                      >
-                        <span>{row[key]}</span>
-                      </div>
-                    </td>
+                      <span>{row[key]}</span>
+                    </div>
                   ))}
-                </tr>
+                </div>
               ))}
-            </tbody>
-          </table>
+            </div>
+          </div>
         )}
       </Droppable>
     </DragDropContext>
